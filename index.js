623665b6eacb25593c2da29e13c9bd989d7aca46
Jessica Chan <jessce3@gmail.com>
2017-10-10 03:38:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidayTo(holiday, name) {
   return(`Happy ${holiday}, ${name}!`)
 }
 
-function holidayCountdown(days, countdown) {
+function holidayCountdown(days, holiday) {
   return(`It's ${days} days until ${holiday}!`)
 }
